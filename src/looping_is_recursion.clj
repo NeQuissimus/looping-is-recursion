ef6e99eb0ef396e4b496a9ba485bfcc539594c22
Tim Steinbach <tsteinbach@trustwave.com>
2014-07-14 08:44:06 -0400

    Ex 3

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -15,7 +15,13 @@
   (lastOne a-seq nil)))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (let [compar (fn [sq1 sq2]
+                  (cond
+                    (and (empty? sq1) (empty? sq2)) true
+                    (or (empty? sq1) (empty? sq2)) false
+                    (not (= (first sq1) (first sq2))) false
+                    :else (recur (rest sq1) (rest sq2))))]
+    (compar seq1 seq2)))
 
 (defn find-first-index [pred a-seq]
   ":(")
